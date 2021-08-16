c7ca6f09c71b0d6b55b96cf6ab324b8bd6e5542e
Nick Cherry <nickcherryjiggz@gmail.com>
2021-08-16 15:36:07 -0400

    redirect to home on 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 
 import { Layout, Seo } from 'components';
+import { navigate } from '@reach/router';
 
-const NotFoundPage: FC = () => (
-  <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-  </Layout>
-);
+const NotFoundPage: FC = () => {
+  useEffect(() => {
+    navigate('/');
+  }, []);
+
+  return (
+    <Layout>
+      <Seo title="404: Not found" />
+      <h1>404: Not Found</h1>
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
