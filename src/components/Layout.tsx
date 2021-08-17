0c5c1700bdb8eed65e3b5e4e213bf6213598395f
Nick Cherry <nickcherryjiggz@gmail.com>
2021-08-17 10:51:34 -0400

    smaller vertical margins for mobile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ export type LayoutProps = {
 };
 
 const Layout: FC<LayoutProps> = ({ children, title }) => (
-  <div className="mx-10 md:mx-32 my-8">
+  <div className="mx-10 md:mx-32 my-4 md:my-8">
     <Seo title={title}>{children}</Seo>
     {children}
   </div>
