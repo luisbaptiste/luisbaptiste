39bc3828d0077578b598c08fc3d866ccd3855f60
Nick Cherry <nickcherryjiggz@gmail.com>
2021-08-17 20:56:35 -0400

    add clicky

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,6 +9,7 @@ export type LayoutProps = {
 const Layout: FC<LayoutProps> = memo(({ children, title }) => (
   <div className="px-4 md:px-28 pt-4 pd:pt-8 flex flex-col h-full justify-between">
     <Seo title={title}>{children}</Seo>
+    <script async src="//static.getclicky.com/101329869.js"></script>
     <main>{children}</main>
     <footer className="text-center text-xs pt-8 pb-32 sm:pb-4">
       <FinePrint>&copy; Luis Baptiste {new Date().getFullYear()}</FinePrint>
